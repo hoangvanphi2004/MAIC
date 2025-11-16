3d05770458bc2db29dbf2f977a2f8a88dd85a6ff
philosophi <hoangvanphi2004@gmail.com>
2025-11-16 21:41:13 +0700

    huh?

--- multigrid_MAAC.py
+++ multigrid_MAAC.py
@@ -266,7 +266,7 @@ if __name__ == '__main__':
                              "model/training contents")
     parser.add_argument("--n_rollout_threads", default=1, type=int)
     parser.add_argument("--buffer_length", default=int(1e6), type=int)
-    parser.add_argument("--n_episodes", default=50000, type=int)
+    parser.add_argument("--n_episodes", default=5000, type=int)
     parser.add_argument("--episode_length", default=40, type=int)
     parser.add_argument("--steps_per_update", default=100, type=int)
     parser.add_argument("--num_updates", default=4, type=int,
