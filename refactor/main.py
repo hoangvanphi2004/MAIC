4f2e1ce15ff7f5d15aa1829ec5b54a05202688e2
philosophi <hoangvanphi2004@gmail.com>
2026-03-14 20:13:40 +0700

    fix man

--- refactor/main.py
+++ refactor/main.py
@@ -6,6 +6,8 @@ from train import run_training
 
 if __name__ == '__main__':
 	parser = argparse.ArgumentParser(description='Train CMASAC with configurable observation mode and information bonus coefficient.')
+	parser.add_argument('--env', type=str, default='MultiGrid-MultiTargetEmpty-16x16-v0', help='Gym environment id (e.g. MultiGrid-PassSparse-8x8-v0).')
+	parser.add_argument('--num_agents', type=int, default=2, help='Number of agents to pass to the environment constructor.')
 	parser.add_argument('--simple', action='store_true', help='Use simple obs/state: obs=[x,y,dir], state=concat of all agent obs.')
 	parser.add_argument('--info_coef', type=float, default=0.0, help='Set CMASAC.scaled_information_gain_coef (e.g. --info_coef 0).')
 	parser.add_argument('--entropy_coef', type=float, default=0.0, help='Set CMASAC.scaled_entropy_coef (e.g. --entropy_coef 0).')
@@ -16,6 +18,8 @@ if __name__ == '__main__':
 	obs_mode = 'simple' if args.simple else 'full'
 
 	run_training(
+		env_id=args.env,
+		num_agents=args.num_agents,
 		obs_state_mode=obs_mode,
 		episodes=5000,
 		steps_per_episode=40,
